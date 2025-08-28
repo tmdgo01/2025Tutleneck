992e0bdc7cce012dabc6856ee83c1ea73d3de097
ssy91 <smi1eday@naver.com>
2025-08-28 14:18:26 +0900

    알람페이지 추가

---
+++ lib/alarm_list_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'alarm_data.dart';
+import 'alarm_screen.dart';
+
+class AlarmListPage extends StatefulWidget {
+  const AlarmListPage({Key? key}) : super(key: key);
+
+  @override
+  State<AlarmListPage> createState() => _AlarmListPageState();
+}
+
+class _AlarmListPageState extends State<AlarmListPage> {
+  List<AlarmData> alarms = [];
+
+  void _addAlarm(AlarmData alarm) {
+    setState(() {
+      alarms.add(alarm);
+    });
+  }
+
+  void _deleteAlarm(int index) {
+    setState(() {
+      alarms.removeAt(index);
+    });
+  }
+
+  void _toggleAlarm(int index, bool value) {
+    setState(() {
+      alarms[index].isAlarmEnabled = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE4F3E1),
+      appBar: AppBar(
+        title: const Text('운동 알람'),
+        backgroundColor: Color(0xFFE4F3E1),
+        elevation: 0,
+      ),
+      body: alarms.isEmpty
+          ? const Center(child: Text('알람이 없습니다. + 버튼으로 추가하세요'))
+          : ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: alarms.length,
+        itemBuilder: (context, index) {
+          final alarm = alarms[index];
+          return Card(
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)),
+            child: ListTile(
+              contentPadding:
+              const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              title: Text(
+                alarm.label ?? '운동 알람',
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              subtitle: Text(
+                '${alarm.startHour.toString().padLeft(2, '0')}:${alarm.startMinute.toString().padLeft(2, '0')}'
+                    ' ~ '
+                    '${alarm.endHour.toString().padLeft(2, '0')}:${alarm.endMinute.toString().padLeft(2, '0')}'
+                    ' | ${alarm.selectedInterval}시간 간격',
+                style: const TextStyle(fontSize: 14),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Switch(
+                    value: alarm.isAlarmEnabled,
+                    onChanged: (val) => _toggleAlarm(index, val),
+                    activeColor: Color(0xFF4CAF50),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteAlarm(index),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF4CAF50),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Alarm(
+                onAlarmCreated: (newAlarm) {
+                  _addAlarm(newAlarm);
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:finalproject/exercise_screen.dart';
+import 'package:finalproject/setting-screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'alarm_list_page.dart';
 import 'daily_screen.dart';
 
 void main() async {
@@ -24,7 +26,8 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(
-            height: 80.0,  // 상단여백
+            height: 50.0,  // 상단여백
+            ///여백 수정
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -35,7 +38,8 @@ class HomeScreen extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(height: 40.0),  // 여백
+          SizedBox(height: 10.0),  // 여백
+          ///사이즈 박스 수정 여백 수정
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 40.0),
             child: Column(
@@ -84,15 +88,53 @@ class HomeScreen extends StatelessWidget {
                       },
                     ),
                     _MenuButton(
-                      icon: Icons.settings,
-                      label: '설정',
+                      icon: Icons.access_alarms_outlined,
+                      label: '알람',
                       color: Color(0xFFF1F3C9),
                       onTap: (){
-                        print('설정 클릭됨!');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AlarmListPage(),
+                          ),
+                        );
                       },
                     ),
                   ],
                 ),
+                ///수정 사항입니다(메뉴버튼 추가, 로고 이동), 버튼 위치 조정
+                SizedBox(height: 30.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _MenuButton(
+                      icon: Icons.settings,
+                      label: '설정',
+                      color: Color(0xFFD2F0DC),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Settingscreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    // Image.asset('asset/sit.png',
+                    //   width: 100.0,)
+                  ],
+                ),
+                ///수정입니다 (수정 사항 끝)
+                ///거북이 위치 확인!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                // SizedBox(height: 100,),
+                //  Row(
+                //    mainAxisAlignment: MainAxisAlignment.end,
+                //    children: [
+                //      Image.asset('asset/bottom.png',
+                //        width: 80.0,)
+                //    ],
+
+                ////////////////////////////////////////////////////////////////////////
               ],
             ),
           ),
