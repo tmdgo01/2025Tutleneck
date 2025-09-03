6c854baf988c07820719698e552b8a493e157dcd
huiwonn <dhc5512@daum.net>
2025-09-03 10:50:53 +0900

    스플래시 화면 구성

---
+++ lib/scr/splash.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'scr/homeScreen.dart';
+import 'dart:async';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFDF8E8),
+      body: Center(
+        child: Lottie.asset(
+          'assets/dh.json',  // GIF를 변환한 Lottie 파일
+          width:300,
+          height: 300,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('메인 화면')),
+    );
+  }
+}
